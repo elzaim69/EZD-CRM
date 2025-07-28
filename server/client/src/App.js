aec7a188c12fdb981b2e4ecc01bc62a3f31225b9
elzaim69 <elzaim24@icloud.com>
2025-07-28 22:08:46 +0200

    Create App.js

---
+++ server/client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Repairpoint24 CRM</h1>
+      <p>Dashboard – Übersicht über deine Kundenkontakte.</p>
+    </div>
+  );
+}
+
+export default App;
